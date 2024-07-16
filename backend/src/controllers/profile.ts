62c8c196fc9a5d084771c9ac58defe8784710d3d
jagannath-swarnkar <jagannath18@navgurukul.org>
2024-07-16 11:59:51 +0800

    fixed build breakings

--- backend/src/controllers/profile.ts
+++ backend/src/controllers/profile.ts
@@ -1,60 +1,60 @@
-import Accounts from "../models/Accounts";
-import Credits from "../models/Credits";
-import Expense from "../models/Expense";
-import { IAggregationResponse } from "../utils/types";
+// import Accounts from "../models/Accounts";
+// import Credits from "../models/Credits";
+// import Expense from "../models/Expense";
+// import { IAggregationResponse } from "../utils/types";
 
-export const getProfile = async (_req: any, _res: any) => {
-	try {
-		const totalSpendsRes: IAggregationResponse[] = await Expense.aggregate([
-			{
-				$match: {
-					userId: _req.tokenData.email
-				}
-			},
-			{
-				$group: {
-					_id: null,
-					total: { $sum: "$amount" }
-				}
-			}
-		]);
-		const totalCreditsRes: IAggregationResponse[] = await Credits.aggregate([
-			{
-				$match: {
-					userId: _req.tokenData.email
-				}
-			},
-			{
-				$group: {
-					_id: null,
-					total: { $sum: "$amount" }
-				}
-			}
-		]);
-		const totalAccountCalcRes = await Accounts.aggregate([
-			{
-				$match: {
-					userId: _req.tokenData.email,
-					$and: [{ accountType: { $ne: "credit_card" } }, { active: true }]
-				}
-			},
-			{
-				$group: {
-					_id: null,
-					total: { $sum: "$amount" }
-				}
-			}
-		]);
+// export const getProfile = async (_req: any, _res: any) => {
+// 	try {
+// 		const totalSpendsRes: IAggregationResponse[] = await Expense.aggregate([
+// 			{
+// 				$match: {
+// 					userId: _req.tokenData.email
+// 				}
+// 			},
+// 			{
+// 				$group: {
+// 					_id: null,
+// 					total: { $sum: "$amount" }
+// 				}
+// 			}
+// 		]);
+// 		const totalCreditsRes: IAggregationResponse[] = await Credits.aggregate([
+// 			{
+// 				$match: {
+// 					userId: _req.tokenData.email
+// 				}
+// 			},
+// 			{
+// 				$group: {
+// 					_id: null,
+// 					total: { $sum: "$amount" }
+// 				}
+// 			}
+// 		]);
+// 		const totalAccountCalcRes = await Accounts.aggregate([
+// 			{
+// 				$match: {
+// 					userId: _req.tokenData.email,
+// 					$and: [{ accountType: { $ne: "credit_card" } }, { active: true }]
+// 				}
+// 			},
+// 			{
+// 				$group: {
+// 					_id: null,
+// 					total: { $sum: "$amount" }
+// 				}
+// 			}
+// 		]);
 
-		console.log("totalAccountCalcRes", totalAccountCalcRes);
+// 		console.log("totalAccountCalcRes", totalAccountCalcRes);
 
-		const totalSpends = totalSpendsRes[0]?.total || 0;
-		const totalCredits = totalCreditsRes[0]?.total || 0;
-		const totalBalance = totalCredits - totalSpends || 0;
-		const totalAccountBalance = totalAccountCalcRes[0]?.total || 0;
-		return _res.status(200).json({ totalSpends, totalCredits, totalBalance, totalAccountBalance });
-	} catch (error) {
-		console.error("getProfile error: ", error);
-		return _res.status(500).json({ message: error.message });
-	}
-};
+// 		const totalSpends = totalSpendsRes[0]?.total || 0;
+// 		const totalCredits = totalCreditsRes[0]?.total || 0;
+// 		const totalBalance = totalCredits - totalSpends || 0;
+// 		const totalAccountBalance = totalAccountCalcRes[0]?.total || 0;
+// 		return _res.status(200).json({ totalSpends, totalCredits, totalBalance, totalAccountBalance });
+// 	} catch (error) {
+// 		console.error("getProfile error: ", error);
+// 		return _res.status(500).json({ message: error.message });
+// 	}
+// };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 import routes from "./routes";
 import { AuthGuard } from "./utils/AuthGuard";
-import Expense from "./models/Expense";
+// import Expense from "./models/Expense";
 require("dotenv").config();
 require("./connections/db");
 
@@ -12,18 +12,18 @@ app.use(express.json({ limit: "50mb" }));
 app.get("/", (_req, _res) => {
 	_res.send("Welcome to the Express API");
 });
-app.patch("/bulk_update", [AuthGuard], async (_req: any, _res: any) => {
-	try {
-		const updated = await Expense.updateMany({}, { userId: _req.tokenData.email });
-		return _res.status(200).json(updated);
-	} catch (error) {
-		console.error("console error: ", error);
-		return _res.status(500).send({
-			message: error.message || "Internal Server Error!",
-			code: 500
-		});
-	}
-});
+// app.patch("/bulk_update", [AuthGuard], async (_req: any, _res: any) => {
+// 	try {
+// 		const updated = await Expense.updateMany({}, { userId: _req.tokenData.email });
+// 		return _res.status(200).json(updated);
+// 	} catch (error) {
+// 		console.error("console error: ", error);
+// 		return _res.status(500).send({
+// 			message: error.message || "Internal Server Error!",
+// 			code: 500
+// 		});
+// 	}
+// });
 
 app.use(routes);
 
